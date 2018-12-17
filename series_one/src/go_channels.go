eef78e296ba81e435b051bd6b18bb39991b1363e
tanalam2411 <tan24alam11@gmail.com>
2018-12-17 13:41:01 +0530

    go channels.

---
+++ series_one/src/go_channels.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+
+func foo(c chan int, someValue int) {
+	c <- someValue * 5
+}
+
+func main() {
+	fooVal := make(chan int)
+
+	// go foo(fooVal, 5)
+	// go foo(fooVal, 3)
+
+	// v1 := <-fooVal
+	// v2 := <-fooVal
+
+	// v1, v2 := <-fooVal, <-fooVal
+
+	// fmt.Println(v1, v2)
+
+
+	for i 
+
+}
