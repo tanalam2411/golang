2be11edd7ed660cda878900a66b53d753bccbbee
tanalam2411 <tan24alam11@gmail.com>
2019-01-04 01:01:16 +0530

    go introduction and variables.

---
+++ src/github.com/tanalam2411/series_two/go_variables.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"fmt"
+)
+
+/*
+
+Agenda:
+* Variable declaration
+* Redeclaration and shadowing
+* Visibility
+* Type conversions
+
+*/
+
+//Variable declared at package level can't use := shorthand, and have to use var varName type format
+// Scope of var:
+// 1) if lower case at module level then the scope is throughout the package
+// 2) if upercase then its accessible everywhere (globally)
+// 3) Block scope - var declared inside function is scoped within that block
+var a float32 = 10
+
+//Var block
+
+// two seperate var blockes have been created just for clearity
+
+var (
+	length int     = 10
+	width  float32 = 32
+)
+
+var (
+	class    string = "A"
+	ip_range string = "0-255"
+)
+
+func main() {
+
+	// Three ways to declare variable
+	// First one
+
+	var i int
+	i = 100
+
+	var j = 200 //type is inferred based on the type of value provided on the  right side
+
+	k := "Hello"
+
+	fmt.Println(i, j, k)
+
+	x := 100 // will set type of x as int
+
+	fmt.Printf("%v, %T \n", x, x)
+
+	x = 100.0 // will set type of x as int
+	fmt.Printf("%v, %T \n", x, x)
+
+	y := 100.0 // will set type of y as float 64
+
+	fmt.Printf("%v, %T \n", y, y)
+
+	// So if we want to create var of type float32 then := wont work, and will have to explicitly give type to var
+
+	var z float32 = 32.4
+
+	fmt.Printf("%v, %T \n", z, z)
+
+	var a float32 = 64 // this is called shadowing as variable a is redeclared inside func main
+
+	fmt.Printf("%v, %T \n", a, a)
+
+	// Naming convention
+	// keep acronym uppercase
+	// e.g theHTTP,
+
+	var fetchURL string = "www.google.com"
+
+	fmt.Printf("%v, %T \n", fetchURL, fetchURL)
+
+}
