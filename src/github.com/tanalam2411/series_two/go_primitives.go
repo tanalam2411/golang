4fd370bec2077c3af42122525f1c6b8476d0f23a
tanalam2411 <tan24alam11@gmail.com>
2019-01-06 15:22:25 +0530

    text, string and rune

--- src/github.com/tanalam2411/series_two/go_primitives.go
+++ src/github.com/tanalam2411/series_two/go_primitives.go
@@ -19,6 +19,10 @@ For more checkout - https://golang.org/pkg/builtin/
 func main() {
 
 	// Boolean types - true/false
+	// Boolean is not an alias of any numeric or other type. So you can't convert to its ay quivalent other type like how you do in Python
+
+	fmt.Println("**************** BOOLEAN ********************")
+
 	var b bool = true
 
 	fmt.Printf("%v, %T \n", b, b)
@@ -33,9 +37,12 @@ func main() {
 	var flag bool
 	fmt.Printf("%v, %T \n", flag, flag)
 
+	fmt.Println("**************** INT ********************")
+
 	// numeric type
 	// Integer type (Signed numbers are both positive nad negative, Unsigned numbers are always positive numbers)
 	// int range:
+	// int 		-   min 32bit
 	// int8		-	-128	    					+127
 	// int16	- 	-32768							+32767
 	// int32	- 	-2 147 483 648					+2 147 483 647          	// Approx neg to pos 2 Billion
@@ -45,6 +52,7 @@ func main() {
 	// uint8	-	0 - 255
 	// uint16	-	0 - 65 535
 	// uint32	-	0 - 4 294 967 295
+	// uint64	-
 
 	// byte - is a unsigned 8bit int type (alias for uint8)
 	// rune - is a alias for int32(represents a unicode point)
@@ -52,7 +60,7 @@ func main() {
 	// Operation on same type of different range will through erro - int + int8 not possible,
 	// so in this senario explicit type cast is required int + int(int8)
 
-	var n1 int = 10
+	var n1 int // default
 	var n2 int8 = 11
 
 	println(n1 + int(n2)) // as n1 + n2 will through error (invalid operation: n1 + n2 (mismatched types int and int8))
@@ -62,13 +70,15 @@ func main() {
 	b2 := 3               // 0011
 	fmt.Println(b1 & b2)  // &(and) when both has 1 it sets 1, so 1010 & 0011 - 0010
 	fmt.Println(b1 | b2)  // |(or) when has atleast one 1s , so 1010 | 0011 - 1011
-	fmt.Println(b1 ^ b2)  // ^(exclusive or) only one should have one 1, so 1010 ^ 0011 - 1001
+	fmt.Println(b1 ^ b2)  // ^(exclusive or - xor) only one should have one 1, so 1010 ^ 0011 - 1001
 	fmt.Println(b1 &^ b2) // &^(and not) if both has 0, so 1010 &^ 0011 - 0100
 
 	// Bit shifting
 	b3 := 8              // 2^3
 	fmt.Println(b3 << 3) // left shifting -> 2^3 * 2^3 = 2^6
 	fmt.Println(b3 >> 3) // right shifting -> 2^3 / 2^3 = 2^0
+
+	fmt.Println("**************** FLOAT ********************")
 
 	// Floating point number
 	// float32 = +or-1.18E-38 to +or-3.4E38  //E-38 -> 10^-3 and E38 -> 10^3, so 1.18E-38 = 1.18*10^-38
@@ -80,7 +90,14 @@ func main() {
 	f1 = 2.1E14
 	fmt.Printf("%v, %T \n", f1, f1)
 
+	fmt.Println("**************** COMPLEX ********************")
+
 	// Complex numbers
+	// Zero value of complex number is = 0 + 0i
+	// complex64 = float32(real) + float32(imag)
+	// complex128 = float64(real) + float64(imag)
+	// Builtin functions - complex, real, imag
+
 	var c1 complex64 = 1 + 2i
 	fmt.Printf("%v, %T \n", c1, c1)
 	fmt.Printf("%v, %T \n", real(c1), real(c1))
@@ -96,5 +113,35 @@ func main() {
 	// Create complex number
 	var c4 complex64 = complex(10, 20)
 	fmt.Printf("%v, %T \n", c4, c4)
+
+	fmt.Println("**************** TEXT ********************")
+
+	// Text types :
+	// 1) String - Any utf-8 character
+	// String in go are aliases of bytes
+	// String are imutable
+
+	s1 := "First"
+	s2 := "Second"
+	fmt.Printf("%v, %T\n", s1, s1)
+	fmt.Printf("%v, %T\n", s1[3], s1[3])         // s1[3] gives ascii val of that char
+	fmt.Printf("%v, %T\n", string(s1[3]), s1[3]) // to get string val, call string func
+
+	fmt.Printf("%v, %T\n", s1+s2, s1+s2)
+
+	by1 := []byte(s1) //convert string to byte
+	fmt.Printf("%v, %T \n", by1, by1)
+
+	// 2) rune - Any utf-32 char
+	// rune is a type alias for int32
+	// single quoiting creates rune type, r1 := 'a' will create r1 of type rune(int32)
+	// look strings.Reader#ReadRune
+
+	r1 := 'a'
+	var r2 rune = 'a'
+	fmt.Printf("%v, %T\n", r1, r1)
+	fmt.Printf("%v, %T\n", r2, r2)
+
+	//todo - learn more about rune
 
 }
