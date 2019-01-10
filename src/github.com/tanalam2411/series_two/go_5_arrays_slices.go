b66f1a4de44f57a74d906416f926f242642b333c
tanalam2411 <tan24alam11@gmail.com>
2019-01-10 23:14:25 +0530

    added arrays and slices.

--- src/github.com/tanalam2411/series_two/go_5_arrays_slices.go
+++ src/github.com/tanalam2411/series_two/go_5_arrays_slices.go
@@ -22,8 +22,11 @@ AGENDA
 
 func main() {
 
+	// Arrays - are of fixed size and has to be known at compile time.
 	// grades := [5]int{90, 93, 97, 85, 66}
 	// ... -> Creates array of size provided num of literals. ({90, 93, 97, 85, 66} -> 5)
+
+	fmt.Println("************** Arrays *******************")
 	grades := [...]int{90, 93, 97, 85, 66}
 
 	fmt.Printf("Grades: %v \n", grades)
@@ -41,5 +44,34 @@ func main() {
 
 	//Get values by index
 	fmt.Printf("Student #1: %v \n", students[1])
+
+	// 2D array
+	var identityMatrix [3][3]int = [3][3]int{[3]int{1, 0, 0}, [3]int{0, 1, 0}, [3]int{0, 0, 1}} // var identityMatrix = [3][3]int{[3]int{1, 0, 0}, [3]int{0, 1, 0}, [3]int{0, 0, 1}} // is also correct as it will assign type based on the data on right side
+	fmt.Printf("identityMatrix: %v \n", identityMatrix)
+
+	// identityMatrix can also be declared as:
+
+	var identityMatrixNew [3][3]int
+	identityMatrixNew[0] = [3]int{1, 0, 0}
+	identityMatrixNew[1] = [3]int{0, 1, 0}
+	identityMatrixNew[2] = [3]int{0, 0, 1}
+	fmt.Printf("identityMatrixNew: %v \n", identityMatrixNew)
+
+	fmt.Println("************************ Arrays in Go are values *******************************")
+
+	a1 := [...]int{1, 2, 3, 4, 5}
+	a2 := a1 // Will create a new array a2 with seperate copy of data. It doesn't creates reference
+	a2[4] = 7
+	fmt.Println(a1)
+	fmt.Println(a2)
+
+	// Create reference(Pointer address) instead of creating a seperate copy. So this will update both a1 and a3
+	a3 := &a1
+	a3[1] = 999
+	fmt.Println(a1)
+	fmt.Println(a3)
+
+	// Slices
+	fmt.Println("************** Slices *******************")
 
 }
