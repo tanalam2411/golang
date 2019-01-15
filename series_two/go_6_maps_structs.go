556915daf495879c05eef41563a0009f9a761b3c
tanalam2411 <tan24alam11@gmail.com>
2019-01-15 23:43:06 +0530

    added protocol buffers, notes, maps and structs

---
+++ series_two/go_6_maps_structs.go
@@ -0,0 +1,18 @@
+package main
+
+import ()
+
+/* AGENDA
+
+- Maps
+  - What are they?
+  - Creating
+  - Manipulation
+
+- Structs
+  - What are they?
+  - Creating
+  - Naming conventions
+  - Embedding
+  - Tags
+*/
