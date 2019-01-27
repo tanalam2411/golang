599d7141c70633172a0772208e185decc98dc610
tanalam2411 <tan24alam11@gmail.com>
2019-01-27 23:56:39 +0530

    added go functions.

---
+++ series_two/go_14_functions.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+/* AGENDA
+- Basic syntax
+- Parameters
+- Return values
+- Anonymous functions
+- Functions as types
+- Methods
+*/
+
+/*
+Entry point is always from main function within main package.
+main function doesn't take any parameter.
+
