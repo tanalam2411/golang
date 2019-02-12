4a404b4077aab062818763c3f44f6b85d5af8888
tanalam2411 <tan24alam11@gmail.com>
2019-02-12 08:06:58 +0530

    arrays.

--- series_4/go_013_arrays.go
+++ series_4/go_013_arrays.go
@@ -14,23 +14,33 @@ import "fmt"
 
 const size int8 = 10
 
-
 type Date struct {
-	year int
+	year  int
 	month int
-	day int
+	day   int
 }
 
 type Person struct {
-	dob Date
+	dob  Date
 	name string
-	age int
+	age  int
 }
-
 
 func main() {
 	printTestResults()
 	readPerson()
+
+	arr1, arr2 := arraySender()
+	arr3 := arr1 // will create a separate copy of arr1 and assign it to arr3, so arr1 will not change by assignment(next line)
+	arr3[0] = 999
+	arr4 := arr2 // arr4 will point to the same addr of arr2, so arr2 will change by assignment(next line)
+	arr4[0] = 1000
+	arrayReceiver(arr3, arr4)
+
+	fmt.Printf("main, arr1: %v, %T, %v \n", arr1, arr1, &arr1)
+	fmt.Printf("main, arr2: %v, %T, %v, %v \n", arr2, arr2, &arr2, &arr4)
+
+	interateArray()
 }
 
 func printTestResults() {
@@ -46,8 +56,16 @@ func printTestResults() {
 	//testResult[i] = 200
 
 	fmt.Printf("testResult: %#v \n\n", testResult)
-	testResult = [10]int{1,2,3,4,5,6,7,8,9,10}
+	testResult = [10]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 
+	fmt.Printf("testResult: %#v, len: %v \n\n", testResult, len(testResult))
+	fmt.Printf("testResult 0-4: %#v \n\n", testResult[0:4])
+
+	var slice1 = testResult[:5]
+	fmt.Printf("slice1: %#v, Type: %T \n\n", slice1, slice1)
+	slice1[0] = 11111
+	fmt.Println("----------- slice1[0] = 11111 // so slice is a reference of the array-------------")
+	fmt.Printf("slice1: %#v \n\n", slice1)
 	fmt.Printf("testResult: %#v \n\n", testResult)
 
 	var compNumbers = [size]complex64{10, 20, 30}
@@ -58,9 +76,37 @@ func printTestResults() {
 	fmt.Printf("city names: %#v\n", cityNames)
 }
 
-
 func readPerson() {
 	var people [10]Person
 
 	fmt.Printf("Person: %#v \n\n", people[0])
+}
+
+func arraySender() ([10]int, *[10]int) {
+
+	arr1 := [10]int{10, 20, 30, 40, 50}
+	arr2 := [10]int{60, 70, 80, 90, 100}
+
+	fmt.Printf("arraySender, arr1: %v, %T, %v \n", arr1, arr1, &arr1)
+	fmt.Printf("arraySender, arr2: %v, %T, %v \n", arr2, arr2, &arr2)
+	return arr1, &arr2
+}
+
+func arrayReceiver(arr1 [10]int, arr2 *[10]int) {
+
+	fmt.Printf("arrayReceiver, arr1: %v, %T, %v \n", arr1, arr1, &arr1)
+	fmt.Printf("arrayReceiver, arr2: %v, %T, %v \n", arr2, arr2, &arr2)
+}
+
+func interateArray() {
+
+	arr1 := [5]int{11, 22, 33, 44, 55}
+
+	for i := 0; i < len(arr1); i++ {
+		fmt.Println(arr1[i])
+	}
+
+	for _, v := range arr1 {
+		fmt.Println(v)
+	}
 }
