5997e5c4da0e67b61f42ebb171aaa094205d445d
tanalam2411 <tan24alam11@gmail.com>
2019-03-02 00:03:15 +0530

    write only channel.

--- series_4/go_017_channels.go
+++ series_4/go_017_channels.go
@@ -35,4 +35,41 @@ func main() {
 
 	fmt.Printf("c: %#v \n", c)
 	fmt.Println("len of c: ", len(c))
+
+
+	fmt.Println("------------------ sendOnlyChannel ---------------")
+	sendOnlyChannel()
+}
+
+
+func sendOnlyChannel() {
+
+	var producerChannel chan int
+	producerChannel = make(chan int, 5)
+
+	canOnlyWrite(producerChannel)
+	fmt.Println("len of producerChannel: ", len(producerChannel))
+
+	for each := range producerChannel {
+		fmt.Println("each: ", each)
+	}
+
+	fmt.Println("len of producerChannel: ", len(producerChannel))
+}
+
+
+func canOnlyWrite(c chan<- int) {
+	/*
+	chan<-  // means you can only to this channel
+	so if we try to read from it, it will throw an error
+	v := <-c
+	or if you pass this channel to another function and that trys to read from it, even then it will throw an error
+
+	channel needs to be closed other wise while iterating it when when there is no element in the channel,
+	the for loop will still be in waiting state as channel is not closed.
+	 */
+	c <- 10
+	c <- 20
+	c <- 30
+	close(c)
 }
