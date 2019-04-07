1dc90dc556560628835b6cde34708097b056cd01
tanalam2411 <tan24alam11@gmail.com>
2019-04-07 23:50:55 +0530

    os stdout

--- series_4/go_045_os_stdout.go
+++ series_4/go_045_os_stdout.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	"io"
+	"log"
+	"os"
+)
+
+func main() {
+	f, err := os.Open("main.go")
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	defer f.Close()
+
+	io.Copy(os.Stdout, f)
+}
