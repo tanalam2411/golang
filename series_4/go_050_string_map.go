63ea12bead313eb9b030c85d2c5041a4bf6627f2
tanalam2411 <tan24alam11@gmail.com>
2019-05-01 23:57:08 +0530

    string map

--- series_4/go_050_string_map.go
+++ series_4/go_050_string_map.go
@@ -1,16 +1,34 @@
 package main
 
-import "strings"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	s := "abc-efg"
 
 	result := strings.Map(mapFunc, s)
+	fmt.Printf("in: %s => out: %s\n", s, result)
 }
 
 
 func mapFunc(in rune) rune {
 	switch in {
+
+	case 'a':
+		return 'e'
+	case 'b':
+		return 'f'
+	case 'c':
+		return 'g'
+	case 'e':
+		return 'a'
+	case 'f':
+		return 'b'
+	case 'g':
+		return 'c'
+
 
 	}
 	return in
