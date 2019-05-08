a478384935e3c56b8e14f0fa2baa8e17d01927da
tanalam2411 <tan24alam11@gmail.com>
2019-05-08 23:33:44 +0530

    cli bytes

--- series_4/go_052_cli_bytes.go
+++ series_4/go_052_cli_bytes.go
@@ -22,4 +22,13 @@ func main() {
 	}
 
 	defer f.Close()
+
+	b.ReadFrom(f)
+	fmt.Println(&b, "This is the end!!!")
+
+	lines := bytes.Split(b.Bytes(), []byte("\n"))
+
+	for n, l := range lines {
+		fmt.Printf("%-4d %s\n", n+1, string(l))
+	}
 }
