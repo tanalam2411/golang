cd71336a48f046f80fcd23b1996f2a58ad510656
tanalam2411 <tan24alam11@gmail.com>
2019-12-09 23:17:28 +0530

    context package.

--- series_3/src/09_the_context_package/main.go
+++ series_3/src/09_the_context_package/main.go
@@ -1,15 +1,30 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"fmt"
 	"log"
+	"os"
 	"time"
 )
 
 func main() {
 	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+
+	// this is call the cancel(and the sleepAndTalk will stop) if user enters anything
+	go func() {
+		s := bufio.NewScanner(os.Stdin)
+		s.Scan()
+		cancel()
+	}()
+
+	// or Just cancel the sleepAndTalk func after a second
+	time.AfterFunc(time.Second, cancel)
+
 	sleepAndTalk(ctx, 5*time.Second, "Hello")
+
 }
 
 
