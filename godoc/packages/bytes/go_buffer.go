7e4a2e0edfe5e67c5f1a230f4618ca9f2e02f0c7
Tanveer Alam <tan24alam11@gmail.com>
2020-05-31 19:06:52 +0530

    go buffer.

--- godoc/packages/bytes/go_buffer.go
+++ godoc/packages/bytes/go_buffer.go
@@ -2,17 +2,29 @@ package main
 
 import (
 	"bytes"
+	"encoding/base64"
 	"fmt"
+	"io"
 	"os"
 )
 
 func main() {
 	exampleOne()
+	readerExample()
 }
 
 func exampleOne() {
 	var b bytes.Buffer  // A Buffer needs no initialization
 	b.Write([]byte("hello "))
-	fmt.Fprintf(&b, "world!")
+	fmt.Fprintf(&b, "world! \n\n")
 	b.WriteTo(os.Stdout)
+}
+
+
+func readerExample() {
+	// A Buffer can turn a string or a []byte into an io.Reader.
+	buf := bytes.NewBufferString("AbCdEfGh==")  // returns &Buffer{buf: []byte(s)}
+	dec := base64.NewDecoder(base64.StdEncoding, buf)
+	fmt.Printf("%", dec)
+	io.Copy(os.Stdout, buf)
 }
